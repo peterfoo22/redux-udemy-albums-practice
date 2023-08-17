560d8a0d1fbb6ebde24d4dff3167f43e32168f62
peterfoo22 <psuh22@gmail.com>
2023-08-17 15:20:00 -0700

    updated with expanded panel

---
+++ src/components/AlbumsList.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const AlbumsList = ({user}) => {
+  return (
+    <div>AlbumsList for {user.name}</div>
+  )
+}
+
+export default AlbumsList;
--- src/components/UsersListItem.js
+++ src/components/UsersListItem.js
@@ -3,6 +3,8 @@ import { GoTrash } from 'react-icons/go'
 import Button from './Button'
 import { removeUser } from '../store'
 import { useThunk } from '../hooks/use-thunk'
+import ExpandablePanel from './ExpandablePanel'
+import AlbumsList from './AlbumsList'
 
 const UsersListItem = ({user}) => {
 
@@ -11,19 +13,17 @@ const UsersListItem = ({user}) => {
   const handleClick =() =>{
     doRemoveUser(user);
   };
+  
+  const header = <>
+      <Button className='mr-3' onClick={handleClick} loading={isLoading} > <GoTrash/></Button>
+      {error && <div> Error Deleting Item</div>}
+      {user.name}
+  </>
 
-  return (
-     <div className='mb-2 border rounded' >
-        <div className='flex p-2 justify-between items-center cursor-pointer'>
-          <div className='flex flex-row items-center justify-between items-center cursor-pointer'>
-            <div className='flex flex-row items-center justify-between'>
-              <Button className='mr-3' onClick={handleClick} loading={isLoading} > <GoTrash/></Button>
-              {error && <div> Error Deleting Item</div>}
-              {user.name}
-            </div>
-          </div>
-        </div>
-     </div>
+  return (  
+   <ExpandablePanel header={header}>
+    <AlbumsList user={user} />
+   </ExpandablePanel>
   )
 }
 
